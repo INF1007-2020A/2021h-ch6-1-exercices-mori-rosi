ebb156ce9c99b95339e3c5f727603f4a7899e94d
Rosie Morissette <rosie.morissette@polymtl.ca>
2021-02-16 12:41:41 -0500

    ex complété 1

--- exercice.py
+++ exercice.py
@@ -7,117 +7,119 @@ def order(values: list = None) -> list:
     if values is None:
          # TODO: demander les valeurs ici
         values = []
-        while len(values) < 3:
+        while len(values) < 10:
              values.append(input("Entrez une valeur:\n"))
         values = values.sort()
 
-    return values
-
-
-    #     valeurs = input("Entrez 10 valeurs:\n")
-    #     print(valeurs)
-    #
-    #     liste = valeurs.split()
-    #     liste_modifiee = liste.sort()
-    #
-    # return liste_modifiee
-
-# #2
-# def anagrams(words: list = None) -> bool:
-#     if words is None:
-#         # TODO: demander les mots ici
-#         mot_1 = list(input("Entrez un mot:\n"))
-#         print(mot_1)
-#         mot_2 = list(input("Entrez un autre mot:\n"))
-#         print(mot_2)
-#
-#         mot_1.sort()
-#         mot_2.sort()
-#
-#         if not mot_1 == mot_2:
-#             return False
+    return values #test failed, retourne None, ne retourne pas de liste ordonnée
 
 
 
 
-        # list_mots = [mot_1, mot_2]
-        # for letter in mot_1:
-        #     liste_mot1.append()
-        #     print(liste_mot1.reverse())
-        #
-        # from collections import deque
-        #
-        # mot_1_mod = deque(mot_1)
-        # for i in range(len(mot_1_mod)):
-        #     print(mot_1_mod.append(mot_1_mod.popleft()))
+#2
+def anagrams(words: list = None) -> bool:
+    if words is None:
+        # TODO: demander les mots ici
+        mot_1 = list(input("Entrez un mot:\n"))
+        print(mot_1)
+        mot_2 = list(input("Entrez un autre mot:\n"))
+        print(mot_2)
+
+        mot_1 = mot_1.sort()
+        mot_2 = mot_2.sort()
+
+        if mot_1 == mot_2:
+            print("Les mots sont des anagrammes")
+            return True #retourne True quand la condition n'est pas respectée
+
+        print("Les mot ne sont pas des anagrammes")
+        return False
+
+
+#3
+def contains_doubles(items: list) -> bool:
+
+    set = {}
+    for elem in list: #comment stocker chaque élément de la liste dans l'ensemble
+        set = set.add(elem)
+
+    if set == list:
+        return False
 
 
 
 
-# #3
-# def contains_doubles(items: list) -> bool:
-#
-#     set = {}
-#     for elem in list:
-#         set = set.add()
-#
-#     if set == list:
-#         return False
+#4
 
-# #4**
-# def best_grades(student_grades: dict) -> dict:
-#     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-#     for nom, note in student_grades.items():
-#         moy = {nom, numpy.mean(note)}
-#     moyenne_pre = numpy.mean()
-#     for nom, moyenne in moy.items():
-#         if moyenne < moyenne_pre:
-#             moyenne_pre=moyenne
-#             continue
-#     meilleure_note = {nom: moyenne}
-#
-#     return meilleure_note
-#
-# #5
-# def frequence(sentence: str) -> dict:
-#     # TODO: Afficher les lettres les plus fréquentes
-#     #       Retourner le tableau de lettres
-#
-#     return {}
-#
-# #6
-# def get_recipes():
-#     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-#     pass
-#
-# #6?
-# def print_recipe(ingredients) -> None:
-#     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-#     pass
+Comment trouver la valeur la plus élevée de mon dictionnaire? -> sort by values
+def best_grades(student_grades: dict) -> dict:
+    # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
+    moy = {}
+    for nom, note in student_grades.items():
+        moy[nom] = numpy.mean(note) #dict des moyennes
+    print(moy)
+
+    return moy
+
+5
+def frequence(sentence: str) -> dict:
+    # TODO: Afficher les lettres les plus fréquentes
+    #       Retourner le tableau de lettres
+    # Comment sort by values un dict?
+    # Est-ce que l'espace compte comme char?
+    list_sentence = []
+    for char in sentence:
+        list_sentence.append(char)
+    occurences = {}
+    for elem in list_sentence:
+        if list_sentence.count(elem) >= 5:
+            occurences[elem] = list_sentence.count(elem)
+    print(occurences)
+
+
+    return occurences
+
+#6
+def get_recipes():
+    # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
+
+    recette = input("Entrez un nom de recette:\n")
+    ingredients = []
+    for i in range(6):
+        ingredients.append(input("Entrez un ingrédient:\n"))
+    livre_recettes = {recette: ingredients}
+    print(livre_recettes)
+
+#6
+def print_recipe(ingredients) -> None:
+    # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
+    recette_req = input("Entrez le nom d'une recette:\n")
+    if recette_req in livre_recettes:
+        print("Les ingrédients de", recette_req, "sont: \n", livre_recettes[recette])
 
 
 def main() -> None:
     print(f"On essaie d'ordonner les valeurs...")
     print(order())
 
-    # print(f"On vérifie les anagrammes...")
-    # anagrams()
-    #
-    # my_list = [3, 3, 5, 6, 1, 1]
-    # print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
+    print(f"On vérifie les anagrammes...")
+    anagrams()
 
-    # grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
-    # best_student = best_grades(grades)
-    # print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
-    #
-    # sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
-    # frequence(sentence)
-    #
-    # print("On enregistre les recettes...")
-    # recipes = get_recipes()
-    #
-    # print("On affiche une recette au choix...")
-    # print_recipe(recipes)
+    my_list = [3, 3, 5, 6, 1, 1]
+    print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
+
+    grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
+    best_student = best_grades(grades)
+    print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
+
+    sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    frequence(sentence)
+
+    print("On enregistre les recettes...")
+    recipes = get_recipes()
+
+    print("On affiche une recette au choix...")
+    print_recipe(recipes)
 
 
 if __name__ == '__main__':
